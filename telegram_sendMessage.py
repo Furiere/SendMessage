6c11676ff36723ab82dcc2c87f54cbef74067a90
Furiere <denis.boldin.hse@gmail.com>
2022-04-24 16:00:43 +0300

    testing cron every minute

--- telegram_sendMessage.py
+++ telegram_sendMessage.py
@@ -3,10 +3,10 @@ import json
 
 """ data = {'sum' : 82500}
 json.dump(data, open('balance.json', 'w')) """
-with open ('balance.json', 'r+') as f:
+with open ('/Users/dboldin/Python/Telegram/SendMessage/balance.json', 'r+') as f:
     data = json.load(f)
-    ts.send(messages=['Отправить Березу 6875 за гитару\nОсталось: {} rub.'.format(data['sum'])])
     data['sum'] -= 6875
+    ts.send(messages=['Отправить Березу 6875 за гитару\nОсталось: {} rub.'.format(data['sum'])])
     print(data['sum'])
     f.seek(0)
     json.dump(data, f)
