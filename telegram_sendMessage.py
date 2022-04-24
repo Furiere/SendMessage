54a99625704da31763d9a24a69c43a253074ec38
Furiere <denis.boldin.hse@gmail.com>
2022-04-24 14:32:08 +0300

    added json dump

--- telegram_sendMessage.py
+++ telegram_sendMessage.py
@@ -1,1 +1,13 @@
-import telegram_send
+import telegram_send as ts
+import json
+
+""" data = {'sum' : 82500}
+json.dump(data, open('balance.json', 'w')) """
+with open ('balance.json', 'r+') as f:
+    data = json.load(f)
+    ts.send(messages=['Отправить Березу 6875 за гитару\nОсталось: {} rub.'.format(data['sum'])])
+    data['sum'] -= 6875
+    print(data['sum'])
+    f.seek(0)
+    json.dump(data, f)
+    f.truncate()
